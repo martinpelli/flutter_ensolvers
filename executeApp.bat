8731d2b5e8ff4569b87a7a284808f2135114a6f6
Martin Pellicer <martinpelli@outlook.com>
2022-01-08 18:41:08 -0300

    Deployed on remote

---
+++ executeApp.bat
@@ -0,0 +1,4 @@
+cd node_ensolvers\src
+start "iexplore.exe" https://ensolvers-app.web.app/#/
+call node index.js
+
--- node_ensolvers/src/database.js
+++ node_ensolvers/src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 async function connect(){
-    await mongoose.connect('mongodb://localhost/flutter-node',{
+    await mongoose.connect('mongodb+srv://pelli:ensolvers@cluster0.t1nbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
         useNewUrlParser: true
     });
     console.log('Database: Connected');
